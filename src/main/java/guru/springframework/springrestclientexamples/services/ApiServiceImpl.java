4e7c639c4f031f30048450dd00a7895f6363551d
krlosh <krlosh@gmail.com>
2018-10-15 11:10:54 +0200

    Externalize api url

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -2,22 +2,28 @@ package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 @Service
 public class ApiServiceImpl implements ApiService {
 
     private RestTemplate restTemplate ;
+    private String apiUrl;
 
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String apiUrl) {
         this.restTemplate = restTemplate;
+        this.apiUrl = apiUrl;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        UserData userData = this.restTemplate.getForObject("http://apifaketory.com/api/user?limit=" +limit, UserData.class);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(this.apiUrl)
+                .queryParam("limit", limit);
+        UserData userData = this.restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
         return userData.getData();
     }
 }
